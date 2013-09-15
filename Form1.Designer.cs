b66184fb5b3719faaa8b296c80a00349304084ee
pawelpq <pawelpq@gmail.com>
2013-09-16 00:29:45 +0200

    dwa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -6,7 +6,7 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+cos dodamop
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
